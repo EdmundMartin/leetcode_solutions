5da51b7a7479f358b993ecede36de75e29d72c23
EdmundMartin <edmartin101@googlemail.com>
2020-01-06 20:31:45 +0000

    added info and speed

--- medium/max_difference_between_node_and_ancestor.py
+++ medium/max_difference_between_node_and_ancestor.py
@@ -1,3 +1,24 @@
+"""
+Given the root of a binary tree, find the maximum value V for which there exists different nodes A
+and B where V = |A.val - B.val| and A is an ancestor of B.
+(A node A is an ancestor of B if either: any child of A is equal to B, or any child of A is an ancestor of B.)
+
+Example 1:
+Input: [8,3,10,1,6,null,14,null,null,4,7,13]
+Output: 7
+Explanation:
+We have various ancestor-node differences, some of which are given below :
+|8 - 3| = 5
+|3 - 7| = 4
+|8 - 1| = 7
+|10 - 13| = 3
+Among all possible differences, the maximum value of 7 is obtained by |8 - 1| = 7.
+
+Note:
+The number of nodes in the tree is between 2 and 5000.
+Each node will have value between 0 and 100000.
+"""
+
 
 class TreeNode:
     def __init__(self, x):
@@ -19,6 +40,8 @@ def recurse_tree(node: TreeNode, min_val: int, max_val: int) -> int:
     return max(current_value, left, right)
 
 
+# Runtime: 40 ms, faster than 51.44% of Python3 online submissions for Maximum Difference Between Node and Ancestor.
+# Memory Usage: 17.7 MB, less than 88.89% of Python3 online submissions for Maximum Difference Between Node and Ancestor.
 class Solution:
     def maxAncestorDiff(self, root: TreeNode) -> int:
         return recurse_tree(root, root.val, root.val)
